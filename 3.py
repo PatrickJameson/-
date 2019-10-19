9b218da366a152522b6ace51ba605122cc3bcaab
Гуренко Дмитрий <55867811+PatrickJameson@users.noreply.github.com>
2019-10-19 09:24:13 +0500

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,14 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 18:11:49) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> a=5.0
+>>> b=7.0
+>>> c=9.0
+>>> P=a+b+c
+>>> p=P/2.0
+>>> S=math.sqrt(p*(p-a)*(p-b)*(p-c))
+>>> print("Perimeter -",P)
+Perimeter - 21.0
+>>> print("Area-",S)
+Area- 17.41228014936585
+>>> 
