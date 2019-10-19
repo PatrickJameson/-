01805f9fb6bf1430817cebce7a75ed36ad3dac27
Гуренко Дмитрий <55867811+PatrickJameson@users.noreply.github.com>
2019-10-19 08:57:06 +0500

    Add files via upload

---
+++ 1задание.py
@@ -0,0 +1,11 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 18:11:49) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> A = "2019-10-19"
+>>> date = A.split("-")
+>>> print (date)
+['2019', '10', '19']
+>>> print (date[2]+"/"+date[1]+date[0])
+19/102019
+>>> print (date[2]+"/"+date[1]+"/"+date[0])
+19/10/2019
+>>> 
---
+++ 2.py
@@ -0,0 +1,13 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 18:11:49) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> A=24
+>>> B=214
+>>> C=2
+>>> print ("Max value:, max(A, B, C))
+       
+SyntaxError: EOL while scanning string literal
+>>> print ("Max value: ", max(A, B, C))
+Max value:  214
+>>> print ("Min value: ", min(A, B, C))
+Min value:  2
+>>> 
