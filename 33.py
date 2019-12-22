0c8bf8cc0a01b804556bfc49c2d106acd1901c74
Гуренко Дмитрий <55867811+PatrickJameson@users.noreply.github.com>
2019-12-22 14:38:20 +0500

    Update 33.py

--- 33.py
+++ 33.py
@@ -1,18 +1,16 @@
 # Дан одномерный массив числовых значений, насчитывающий N элементов.
-# Поменять местами элементы, стоящие на чётных и нечётных местах: A[1] ↔ A[2]; A[3] ↔ A[4] ...
+# Выполнить перемещение элементов массива по кругу вправо, т. е. A[1] → A[2]; A[2] → A[3]; ... A[n] → A[1].
 
 import random
-import sys
-n = 25
-a = [random.randint(0, 100) for i in range(0,n)]
+N = int(input("Введите количество элементов массива "))
+a = [random.randint(0, 100) for i in range(0,N)]
 print(a)
-
 if (len(a) % 2 == 1):
-    end = n
+    end = N
 else:
-    end = n-1
+    end = N-1
 
 
-for i in range(0, end-1, 2):
+for i in range(end-1):
     a[i], a[i + 1] = a[i + 1], a[i]
 print(a)
