ed64c12d02fe600e4f14f4955fda4f8db32951d3
Гуренко Дмитрий <55867811+PatrickJameson@users.noreply.github.com>
2019-12-26 18:45:07 +0500

    Create 77.py

---
+++ 77.py
@@ -0,0 +1,64 @@
+# Создать прямоугольную матрицу A, имеющую N строк и M столбцов со случайными элементами.
+# Просуммировать элементы каждой строки матрицы с соответствующими элементами L-й строки.
+
+import random
+
+N = 5  # строк
+M = 7  # столбцов
+
+K = 2
+
+def get_row(column):
+    col = []
+    for i in range(0, column):
+        col.append(random.randint(0, 9))
+
+    return col
+
+def get_matrix(row, column):
+    matrix = []
+    for i in range(0, row):
+        matrix.append(get_row(column))
+
+    return matrix
+
+def print_matrix(matrix):
+    i = 0
+    while i < len(matrix):
+        j = 0
+        row = matrix[i]
+        while j < len(row):
+            column = row[j]
+            print(column, end=' ')
+            j += 1
+
+        print()
+        i += 1
+
+def get_column(matrix, index):
+    column = []
+    i = 0
+    while i < len(matrix):
+        column.append(matrix[i][index])
+        i += 1
+
+    return column
+
+A = get_matrix(N, M)
+print("Исходная матрица:")
+print_matrix(A)
+
+k_column = get_column(A, K - 1)
+
+i = 0
+while i < len(A):
+    j = 0
+
+    while j < len(A[i]):
+        A[i][j] *= k_column[i]
+        j += 1
+
+    i += 1
+
+print("Моифицированная матрица:")
+print_matrix(A)
